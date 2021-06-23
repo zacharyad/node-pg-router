8d69fd05df0d809b41f9c87007b1d8a60a086430
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2021-06-23 18:27:59 -0400

    Removing code for starting-point to be set up!

--- express-routes-REST/app.js
+++ express-routes-REST/app.js
@@ -1,19 +1,44 @@
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use('/api', require('./api'));
-
 app.get('/', (req, res, next) => {
   res.send('<h1>Welcome to the Home Page!</h1>');
 });
 
+app.get('/users/', (req, res, next) => {
+  res.send('<h1>Welcome to the All Users Page!</h1>');
+});
+
+app.get('/users/:id', (req, res, next) => {
+  res.send('<h1>Welcome to the Single Users Page!</h1>');
+});
+
+app.post('/users/', (req, res, next) => {
+  res.json('You tried to create a user');
+});
+
+app.put('/users/:id', (req, res, next) => {
+  res.json('You tried to update a user');
+});
+
+app.delete('/users/:id', (req, res, next) => {
+  res.json('You tried to delete a user');
+});
+
+app.get('/dogs/', (req, res, next) => {
+  res.send('<h1>Welcome to the All Dogs Page!</h1>');
+});
+
+app.get('/dogs/:id', (req, res, next) => {
+  res.send('<h1>Welcome to the Single Dog Page!</h1>');
+});
+
+// What is this????
 app.use((req, res, next) => {
   res.status(404).send('<h1> I do not have a mapping for this route :|</h1>');
 });
 
+// And what is this???
 app.use((err, req, res, next) => {
   console.log(err.stack);
   res.status(err.status || 500).send(err.message);
--- node-pg/app.js
+++ node-pg/app.js
@@ -1,7 +1,3 @@
-const pg = require('pg');
-const postgresUrl = 'postgres://localhost/pg-node-rest';
-const client = new pg.Client(postgresUrl);
-
 const prompt = (data) => {
   process.stdout.write(
     `Main Menu\nCommands:\n1. Select all users\n2. Select all puppies\n3. Exit\n> `
@@ -13,29 +9,10 @@ const main = async (data) => {
   const [command, arg] = entry.split(' ');
   switch (command) {
     case '1':
-      try {
-        const users = await client.query('SELECT * FROM users;');
-        users.rows.forEach((user) => {
-          console.log(`User name: ${user.name}`);
-        });
-      } catch (err) {
-        console.log('Error when trying to retrieve all users');
-        console.log(err);
-      }
       break;
     case '2':
-      try {
-        const dogs = await client.query('SELECT * FROM dogs;');
-        dogs.rows.forEach((dog) => {
-          console.log(`Dog name: ${dog.name}`);
-        });
-      } catch (err) {
-        console.log('Error when trying to retrieve all dogs');
-        console.log(err);
-      }
       break;
     case '3':
-      await client.end();
       process.exit(1);
     default:
       prompt('not valid command');
@@ -44,8 +21,6 @@ const main = async (data) => {
 };
 
 function start() {
-  client.connect();
-  console.log('client has connected!\n');
   prompt();
   process.stdin.on('data', main);
 }
